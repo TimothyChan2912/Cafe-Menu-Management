6b0e5da81a20705a83689452c0d556212617f235
p0htahtoe <semverkerk.1104@gmail.com>
2024-11-20 11:12:07 -0800

    getters and setters for menu items

--- src/DinerMenuItem.java
+++ src/DinerMenuItem.java
@@ -1,11 +1,11 @@
-import java.io.Serializable;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class DinerMenuItem implements MenuItem, Serializable  {
-	
-	private static final long serialVersionUID = 1L;
-	
+    
+    private static final long serialVersionUID = 1L;
+    
     private String title;
     private String itemID;
     private String description;
@@ -16,5 +16,76 @@ public class DinerMenuItem implements MenuItem, Serializable  {
     
     private ArrayList<MenuItem> menuItems = new ArrayList<>(); // Store menu items
 
-   	// xxx your codes
+    //getters
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+    @Override
+    public String getItemID() {
+        return this.itemID;
+    }
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+    @Override
+    public float getPrice() {
+        return this.price;
+    }
+    @Override
+    public int getCount() {
+        return this.count;
+    }
+    @Override
+    public String getMenuType() {
+        return "Diner";
+    }
+    @Override
+    public boolean isAvailable() {
+        return available;
+    }
+    @Override
+    public boolean isCurrent() {
+        return current;
+    }
+
+    //setters
+    @Override
+    public void setCount(int count) {
+        this.count = count;
+    }
+    @Override
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    @Override
+    public void setCurrent(boolean current) {
+        this.current = current;
+    }
+    
+    //other methods
+    @Override
+    public String toDataString() {
+        String dataString = "Diner;" + this.title + ";" + this.itemID + ";" + this.description + ";" + this.price + ";" + this.count + ";" + this.available;
+        return dataString;
+    }
+    @Override
+    public void addItem(MenuItem item) {
+        menuItems.add(item);
+    }
+    @Override
+    public void removeItem(String itemID) {
+        menuItems.remove(itemID);
+    }
+
+    @Override
+    public Iterator<MenuItem> createIterator() {
+        return menuItems.iterator();
+    }
+
+    public int compareTo (MenuItem m) {
+        return this.compareTo(m);
+    }
 }
+
--- src/PancakeMenuItem.java
+++ src/PancakeMenuItem.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class PancakeMenuItem implements MenuItem, Serializable {
-	private static final long serialVersionUID = 1L;
-	
+    private static final long serialVersionUID = 1L;
+    
     private String title;
     private String itemID;
     private String description;
@@ -24,5 +24,76 @@ public class PancakeMenuItem implements MenuItem, Serializable {
         this.current = current;
         this.available = count > 0 && current;
     }
-   		// xxx your codes
+    //getters
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+    @Override
+    public String getItemID() {
+        return this.itemID;
+    }
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+    @Override
+    public float getPrice() {
+        return this.price;
+    }
+    @Override
+    public int getCount() {
+        return this.count;
+    }
+    @Override
+    public String getMenuType() {
+        return "Pancake";
+    }
+    @Override
+    public boolean isAvailable() {
+        return available;
+    }
+    @Override
+    public boolean isCurrent() {
+        return current;
+    }
+
+    //setters
+    @Override
+    public void setCount(int count) {
+        this.count = count;
+    }
+    @Override
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    @Override
+    public void setCurrent(boolean current) {
+        this.current = current;
+    }
+    
+    //other methods
+    @Override
+    public String toDataString() {
+        String dataString = "Pancake;" + this.title + ";" + this.itemID + ";" + this.description + ";" + this.price + ";" + this.count + ";" + this.available;
+        return dataString;
+    }
+    @Override
+    public void addItem(MenuItem item) {
+        menuItems.add(item);
+    }
+    @Override
+    public void removeItem(String itemID) {
+        menuItems.remove(itemID);
+    }
+
+    @Override
+    public Iterator<MenuItem> createIterator() {
+        return menuItems.iterator();
+    }
+
+    public int compareTo (MenuItem m) {
+        return this.compareTo(m);
+    }
 }
+
