d6403efbbb1a73906d7ed72306400057666a00f2
TimothyChan2912 <tchan110559@gmail.com>
2024-12-04 23:30:40 -0800

    Started Admin Customer Manager (in progress)

---
+++ src/AdminCustomerManager.java
@@ -0,0 +1,116 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class AdminCustomerManager extends JFrame {
+    private final int FRAME_WIDTH = 1000;
+    private final int FRAME_HEIGHT = 1000;
+
+    private JLabel adminInfo;
+    private JLabel inactiveCustomers;
+    private JLabel activeCustomers;
+    private JLabel sortOrder;
+    private JLabel SearchBy;
+
+
+    private JButton btnReactivate;
+    private JButton btnInactivate;
+    private JButton btnAdd;
+    private JButton btnEdit;
+    private JButton btnDelete;
+    private JButton btnSort;
+
+    private JComboBox<String> filterOrder;
+    private JComboBox<String> filterCriteria;
+
+    private GridBagLayout layout = new GridBagLayout();
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private AddObjects a = new AddObjects();
+
+    private JList<String> inactiveCustomerListDisplay;
+    private JList<String> activeCustomerListDisplay;
+
+    public ArrayList<String> activeCustomerList;
+    public ArrayList<String> inactiveCustomerList;
+
+    public AdminCustomerManager(Admin admin) {
+        btnReactivate = new JButton("Re-activate");
+        btnInactivate = new JButton("Inactivate");
+        btnAdd = new JButton("Add");
+        btnEdit = new JButton("Edit");
+        btnDelete = new JButton("Delete");
+        btnSort = new JButton("Sort");
+
+        adminInfo = new JLabel(admin.getFirstName() + " " + admin.getLastName() + ": " + admin.getUserName());
+        inactiveCustomers = new JLabel("Cafe Inactive Customers:");
+        activeCustomers = new JLabel("Cafe Active Customers:");
+        sortOrder = new JLabel("Sort Order:");
+        SearchBy = new JLabel("Search/Sort By:");
+
+        filterOrder = new JComboBox<String>();
+        filterCriteria = new JComboBox<String>();
+
+        filterOrder.addItem("Ascending");
+        filterOrder.addItem("Descending");
+
+        filterCriteria.addItem("First Name");
+        filterCriteria.addItem("Last Name");
+        filterCriteria.addItem("Email");
+        filterCriteria.addItem("Username");
+
+        BtnListener btnlistener = new BtnListener();
+        btnReactivate.addActionListener(btnlistener);
+        btnInactivate.addActionListener(btnlistener);
+        btnAdd.addActionListener(btnlistener);
+        btnEdit.addActionListener(btnlistener);
+        btnDelete.addActionListener(btnlistener);
+        btnSort.addActionListener(btnlistener);
+
+        JPanel pAdminCustomerManager = new JPanel();
+        pAdminCustomerManager.setLayout(layout);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        a.addObjects(adminInfo, pAdminCustomerManager, layout, gbc, 0, 0, 1, 1, 50, 50);
+        a.addObjects(inactiveCustomers, pAdminCustomerManager, layout, gbc, 0, 1, 1, 1, 50, 50);
+        a.addObjects(activeCustomers, pAdminCustomerManager, layout, gbc, 1, 1, 1, 1, 50, 50);
+        a.addObjects(btnReactivate, pAdminCustomerManager, layout, gbc, 0, 2, 1, 1, 50, 25);
+        a.addObjects(btnInactivate, pAdminCustomerManager, layout, gbc, 1, 2, 1, 1, 50, 25);
+        a.addObjects(btnAdd, pAdminCustomerManager, layout, gbc, 0, 3, 1, 1, 50, 25);
+        a.addObjects(btnEdit, pAdminCustomerManager, layout, gbc, 1, 3, 1, 1, 50, 25);
+        a.addObjects(btnDelete, pAdminCustomerManager, layout, gbc, 2, 3, 1, 1, 50, 25);
+        a.addObjects(SearchBy, pAdminCustomerManager, layout, gbc, 0, 4, 1, 1, 50, 50);
+        a.addObjects(filterCriteria, pAdminCustomerManager, layout, gbc, 1, 4, 1, 1, 50, 25);
+        a.addObjects(sortOrder, pAdminCustomerManager, layout, gbc, 0, 5, 1, 1, 50, 50);
+        a.addObjects(filterOrder, pAdminCustomerManager, layout, gbc, 1, 5, 1, 1, 50, 25);
+        a.addObjects(btnSort, pAdminCustomerManager, layout, gbc, 2, 5, 1, 1, 50, 25);
+
+        this.add(pAdminCustomerManager);
+        this.setTitle("Customer Management Dashboard");
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    class BtnListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == btnReactivate) {
+                // Reactivate customer
+            } else if(e.getSource() == btnInactivate) {
+                // Inactivate customer
+            } else if(e.getSource() == btnAdd) {
+                // Add customer
+            } else if(e.getSource() == btnEdit) {
+                // Edit customer
+            } else if(e.getSource() == btnDelete) {
+                // Delete customer
+            } else if(e.getSource() == btnSort) {
+                // Sort customers
+            }
+        }
+    }
+
+
+}
---
+++ src/AdminCustomerManagerAdd.java
@@ -0,0 +1,125 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.JFrame;
+
+
+public class AdminCustomerManagerAdd extends JFrame {
+    private final int FRAME_WIDTH = 1000;
+    private final int FRAME_HEIGHT = 250;
+
+    private JLabel userType;
+    private JLabel firstName;
+    private JLabel lastName;
+    private JLabel email;
+    private JLabel password;
+    private JLabel status;
+
+    private JButton btnOk;
+    private JButton btnCancel;
+
+    private JTextField firstNameText;
+    private JTextField lastNameText;
+    private JTextField emailText;
+    private JTextField passwordText;
+
+    private ButtonGroup statusCheck;
+    private JRadioButton statusCheckActive;
+    private JRadioButton statusCheckInactive;   
+
+    private JComboBox<String> userTypeList;
+
+    private GridBagLayout layout = new GridBagLayout();
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private AddObjects a = new AddObjects();
+
+    public AdminCustomerManagerAdd() {
+        btnOk = new JButton("Ok");
+        btnCancel = new JButton("Cancel");
+
+        BtnListener btnlistener = new BtnListener();
+        btnOk.addActionListener(btnlistener);
+        btnCancel.addActionListener(btnlistener);
+
+        userType = new JLabel("User Type:");
+        firstName = new JLabel("First Name:");
+        lastName = new JLabel("Last Name:");
+        email = new JLabel("Main ID:");
+        password = new JLabel("Password:");
+        status = new JLabel("Status:");
+
+        firstNameText = new JTextField("");
+        lastNameText = new JTextField("");
+        emailText = new JTextField("");
+        passwordText = new JTextField("");
+
+        statusCheck = new ButtonGroup();
+        statusCheckActive = new JRadioButton("Active");
+        statusCheckInactive = new JRadioButton("Inactive");
+
+        userTypeList = new JComboBox<String>();
+        userTypeList.addItem("Admin");
+        userTypeList.addItem("Customer");
+        
+        statusCheck.add(statusCheckActive);
+        statusCheck.add(statusCheckInactive);
+
+        JPanel pAdd = new JPanel();
+        pAdd.setLayout(layout);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        a.addObjects(userType, pAdd, layout, gbc, 0, 0, 1, 1, 50, 50);
+        a.addObjects(firstName, pAdd, layout, gbc, 0, 1, 1, 1, 50, 50);
+        a.addObjects(lastName, pAdd, layout, gbc, 0, 2, 1, 1, 50, 50);
+        a.addObjects(email, pAdd, layout, gbc, 0, 3, 1, 1, 50, 50);
+        a.addObjects(password, pAdd, layout, gbc, 0, 4, 1, 1, 50, 50);
+        a.addObjects(status, pAdd, layout, gbc, 0, 5, 1, 1, 50, 50);
+
+        a.addObjects(firstNameText, pAdd, layout, gbc, 1, 1, 3, 1, 250, 25);
+        a.addObjects(lastNameText, pAdd, layout, gbc, 1, 2, 3, 1, 250, 25);
+        a.addObjects(emailText, pAdd, layout, gbc, 1, 3, 3, 1, 250, 25);
+        a.addObjects(passwordText, pAdd, layout, gbc, 1, 4, 3, 1, 250, 25);
+        a.addObjects(statusCheckActive, pAdd, layout, gbc, 1, 5, 1, 1, 50, 25);
+        a.addObjects(statusCheckInactive, pAdd, layout, gbc, 2, 5, 1, 1, 50, 25);
+        a.addObjects(userTypeList, pAdd, layout, gbc, 1, 0, 1, 1, 50, 25);
+
+        gbc.ipady = 50;
+
+        a.addObjects(btnOk, pAdd, layout, gbc, 0, 6, 1, 1, 50, 25);
+        a.addObjects(btnCancel, pAdd, layout, gbc, 1, 6, 3, 1, 50, 25);
+
+        this.add(pAdd);
+        this.setTitle("Enter User Details");
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    class BtnListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == btnOk) {
+                // Add user to database
+            } else if (e.getSource() == btnCancel) {
+                dispose();
+            }
+        }
+    }
+
+    public void addUser() {
+        String userType = userTypeList.getSelectedItem().toString();
+        String firstName = firstNameText.getText();
+        String lastName = lastNameText.getText();
+        String email = emailText.getText();
+        String password = passwordText.getText();
+        boolean isActive = statusCheckActive.isSelected();
+
+        if (isActive) {
+            // Add user to database
+        } else {
+            // Add user to database   
+        }  
+    }
+}
