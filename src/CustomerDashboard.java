4d38ffc297aaa0559f151ceb4cff351470b6adb9
p0htahtoe <semverkerk.1104@gmail.com>
2024-12-06 13:54:46 -0800

    *unfinished* Customer Dashboard

--- src/CustomerDashboard.java
+++ src/CustomerDashboard.java
@@ -1,11 +1,6 @@
+import java.awt.*;
 import javax.swing.*;
 import javax.swing.text.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -31,5 +26,93 @@ public class CustomerDashboard extends JFrame {
 	private JRadioButton fifteenPercentButton;
 	private JRadioButton twentyPercentButton;
 
-   	// xxx your codes
+	private JButton btnLogout;
+	private JButton btnAddToCart;
+	private JButton btnPlaceOrder;
+	private JButton btnCancelOrder;
+	private JButton btnSort;
+	private JButton btnSearch;
+
+	private JLabel cartLabel;
+	private JLabel billLabel;
+	private JLabel menuLabel;
+	private JLabel userInfo;
+	private JLabel sortOrder;
+	private JLabel searchSortBy;
+
+	private JComboBox<String> sortOrderDropDown;
+	private JComboBox<String> sortByDropDown;
+	private JTextField searchField;
+
+	private GridBagLayout layout = new GridBagLayout();
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private AddObjects a = new AddObjects();
+
+   	public CustomerDashboard (User currentUser) {
+		this.currentUser = currentUser;
+
+		userInfo = new JLabel(currentUser.getFirstName() + " " + currentUser.getLastName() + ": " + currentUser.getUserName());
+		cartLabel = new JLabel("Cart");
+		billLabel = new JLabel("Bill");
+		menuLabel = new JLabel("Cafe Menu");
+		sortOrder = new JLabel("Sort Order:");
+		searchSortBy = new JLabel("Search/Sort By:");
+			
+		btnAddToCart = new JButton("Add to Cart");
+		btnCancelOrder = new JButton("Cancel");
+		btnPlaceOrder = new JButton("Order");
+		btnLogout = new JButton("Logout");
+		btnSort = new JButton("Sort");
+		btnSearch = new JButton("Search");
+
+		sortOrderDropDown = new JComboBox<String>();
+        sortByDropDown = new JComboBox<String>();
+
+        sortOrderDropDown.addItem("Ascending");
+        sortOrderDropDown.addItem("Descending");
+		sortByDropDown.addItem("Title");
+		sortByDropDown.addItem("ID");
+		sortByDropDown.addItem("Description");
+        sortByDropDown.addItem("Price");
+		sortByDropDown.addItem("Count");
+		sortByDropDown.addItem("Menu Type");
+		sortByDropDown.addItem("Current");
+		sortByDropDown.addItem("Available");
+        	
+
+		breakfastCheckbox = new JCheckBox("Breakfast/Lunch");
+		dinnerCheckbox = new JCheckBox("Dinner");
+
+		BtnListener btnlistener = new BtnListener();
+        btnAddToCart.addActionListener(btnlistener);
+        btnCancelOrder.addActionListener(btnlistener);
+        btnPlaceOrder.addActionListener(btnlistener);
+        btnSort.addActionListener(btnlistener);
+		btnSearch.addActionListener(btnlistener);
+		btnLogout.addActionListener(btnlistener);
+	}
+
+	class BtnListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == btnLogout) {
+				new CafeOnlineOrderSystemGUI();
+				dispose();
+			}
+			else if (e.getSource() == btnAddToCart) {
+				// Add the selected item to the cart
+			}
+			else if (e.getSource() == btnCancelOrder) {
+				// Cancel the order
+			}
+			else if (e.getSource() == btnPlaceOrder) {
+				// Place the order
+			}
+			else if (e.getSource() == btnSort) {
+				// Sort the menu items
+			}
+			else if (e.getSource() == btnSearch) {
+				// Search for menu items
+			}
+		}
+	}
 }
